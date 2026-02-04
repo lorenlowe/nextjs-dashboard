d5da1b47e1fa3ab56ae4077d2073fe2f169fbf7f
Loren M. Lowe <lorenlowe@etcmail.com>
2026-02-04 07:25:07 -0500

    Changes from Vercel tutorial

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function CustomersPage() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <p>Customers Page</p>
+    </main>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function InvoicesPage() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <p>Invoices Page</p>
+    </main>
+  );
+}
